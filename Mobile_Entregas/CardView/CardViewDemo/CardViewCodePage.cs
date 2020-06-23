bea95644327c57ee544a43c60ae52dc7e2521661
ingrideme <ingrid_melo9@hotmail.com>
2020-06-23 14:40:12 -0300

    App_Cardview
    cardview com xaml, control template e codigo

---
+++ Mobile_Entregas/CardView/CardViewDemo/CardViewCodePage.cs
@@ -0,0 +1,69 @@
+﻿using CardViewDemo.Controls;
+using Xamarin.Forms;
+
+namespace CardViewDemo
+{
+    public class CardViewCodePage : ContentPage
+    {
+        public CardViewCodePage()
+        {
+            Title = "CardView Code Demo";
+            Padding = 10;
+
+            StackLayout layout = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.Fill,
+                Children =
+                {
+                    new CardView
+                    {
+                        BorderColor = Color.DarkGray,
+                        CardTitle = "Maycon Representante",
+                        CardDescription = "O melhor representante que a 2ND já viu",
+                        IconBackgroundColor = Color.SlateGray,
+                        IconImageSource = ImageSource.FromFile("user.png")
+
+                    },
+                    new CardView
+                    {
+                        BorderColor = Color.DarkGray,
+                        CardTitle = "Ingrid Melo",
+                        CardDescription = "Indescritivel.",
+                        IconBackgroundColor = Color.SlateGray,
+
+                        IconImageSource = ImageSource.FromFile("user.png")
+
+                    },
+                    new CardView
+                    {
+                        BorderColor = Color.DarkGray,
+                        CardTitle = "Rafael Conrado",
+                        CardDescription = "Trouxe o cafézinho?",
+                        IconBackgroundColor = Color.SlateGray,
+                                           IconImageSource = ImageSource.FromFile("user.png")
+
+                    },
+                    new CardView
+                    {
+                        BorderColor = Color.DarkGray,
+                        CardTitle = "Tiago",
+                        CardDescription = "Blablablablablablablablablablablabla",
+                        IconBackgroundColor = Color.SlateGray,
+                                                IconImageSource = ImageSource.FromFile("user.png")
+
+                    },
+                }
+            };
+
+            ScrollView scroll = new ScrollView
+            {
+                HorizontalOptions = LayoutOptions.Fill,
+                VerticalOptions = LayoutOptions.Fill,
+                Content = layout
+            };
+
+            Content = scroll;
+        }
+    }
+}
